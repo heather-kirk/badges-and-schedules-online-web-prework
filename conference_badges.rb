994acc0ed0dba9e49a82322d4cc6453e34594555
Heather Kirk <hskirk@gmail.com>
2019-01-13 06:30:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |array2|
+  array.push("Hello, my name is #{new_array}")
+end
+ return new_array
+end
+
+def assign_rooms(names)
+  rooms = []
+  count = 0
+  names.each do |array2|
+  array.push("Hello #{name}! You'll be assigned to room #{rooms}!")
+end
+return rooms
+count += 1 
+end
+
+def printer
+  batch_badge_creator
+  assign_rooms
+end
